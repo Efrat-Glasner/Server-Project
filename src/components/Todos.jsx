546de5465a4a77dcffbbd6430671d10800f3319e
Efrat-Glasner <efratg2005@gmail.com>
2024-12-24 16:00:11 +0200

    start todos

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,6 +1,10 @@
+import { useEffect, useContext } from "react";
+import { CurrentUser } from "../App";
 function Todos() {
-    return(
+    const [currentUser, setCurrentUser] = useContext(CurrentUser);
+    const response = await fetch("http://localhost:3000/todos");
+    return (
         <h1>Todos</h1>
-    ) 
+    )
 }
 export default Todos;
