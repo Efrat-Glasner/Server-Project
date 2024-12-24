41ffc521b8aab8afc3c690e95faf288a429e5746
yehudit@009 <judith.sabag1@gmail.com>
2024-12-24 10:51:29 +0200

    info change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 // import React from "react";
-import { Link, useNavigate, Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { useContext, useState } from "react";
 
 import "../css/home.css";
